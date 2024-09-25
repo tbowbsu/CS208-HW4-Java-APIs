5f9d37fe193b720b91ad56439372355c26e8cdac
Tony B <tonybowen@u.boisestate.edu>
2024-09-25 14:52:04 -0700

    Implemented display_name with two search queries

--- src/main/java/cs208/PracticeController.java
+++ src/main/java/cs208/PracticeController.java
@@ -1,11 +1,34 @@
 package cs208;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PracticeController
 {
     // TODO: create a GET route with two query parameters
+    @GetMapping("/display_name")
+    String getSearchWithTwoParameters(
+            @RequestParam("first_name") String firstName,
+            @RequestParam("last_name") String lastName
+    )
+    {
+        System.out.println("PracticeController.getSearchWithTwoParameters - START");
+        System.out.println("first name sent in the browser URL = " + firstName);
+        System.out.println("last name sent in the browser URL = " + lastName);
+        System.out.println("PracticeController.getSearchWithTwoParameters - END");
+
+        String valueReturnedToBrowser =
+                "These are the values of parameters passed in the URL:<br>" +
+                        "parameter1 = " + firstName + "<br>" +
+                        "parameter2 = " + lastName + "<br><br>" +
+                        "NOTE: the parameters are separated in the URL by an ampersand (&amp;) character.<br><br>" +
+                        "<hr>";
+                        //"Find me in the TutorialController.getSearchWithTwoParameters() method";
+
+        return valueReturnedToBrowser;
+    }
 
 
     // TODO: create a GET route with a path parameter
